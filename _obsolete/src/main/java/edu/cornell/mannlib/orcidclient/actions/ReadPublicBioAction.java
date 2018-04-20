4d4dc9ec08eb449fd025f978923f3a589af1478e
Jim Blake <jeb228@cornell.edu>
2018-04-20 16:47:27 -0400

    Remove most of the _obsolete area

--- _obsolete/src/main/java/edu/cornell/mannlib/orcidclient/actions/ReadPublicBioAction.java
+++
@@ -1,8 +0,0 @@
-package edu.cornell.mannlib.orcidclient.actions;
-
-import edu.cornell.mannlib.orcidclient.OrcidClientException;
-import edu.cornell.mannlib.orcidclient.model.OrcidProfile;
-
-public interface ReadPublicBioAction {
-    OrcidProfile execute(String orcid) throws OrcidClientException;
-}
